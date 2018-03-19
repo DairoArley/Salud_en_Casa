91eca4213f79a984b0ca9393ba7c9045841c8946
jhonatanglorys <jhonatanglorys@gmail.com>
2018-03-18 22:28:52 -0500

    Select field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './superusercountry/description-completed-services.html',
+  templateUrl: './superusercountry/generate-report.html',
   //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
